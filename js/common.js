96305de6ac1176cd2ca3a6d3e25d442496f90773
ssunday0611 <colorpicker06@gmail.com>
2024-04-09 14:16:18 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -236,7 +236,7 @@ $(document).ready(function () {
       downloadURI(myImage, "416_image.png");
 
       alert("그려");
-      // var win = window.open();
+      var win = window.open();
       win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0;top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen> </iframe>');
     });
     $(".preview_typing").css("border","0.2rem solid #DDDDDD");
